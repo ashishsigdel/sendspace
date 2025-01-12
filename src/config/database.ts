39f2d23cf9b11c73cd909af60a2591c15aee15f1
ashish <a.asis.sigdel01@gmail.com>
2025-01-12 14:33:20 +0545

    updated with environment variable

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,10 +2,10 @@ import { Sequelize } from "sequelize-typescript";
 import Session from "@/models/session";
 
 const sequelize: any = new Sequelize({
-  host: "localhost",
-  username: "root",
-  password: "rocket5%",
-  database: "sendspace",
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   dialect: "mysql",
   dialectModule: require("mysql2"),
   logging: false,
